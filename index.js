9e23907a8d9558a66c0d25484cf81ff2317f5577
Maverick Tesini <mavericktesini@gmail.com>
2017-11-09 14:37:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+// Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  } else {
+    return 'I can not accept that currency';
+  }
+}
+
+function ternaryCheckCity(city) {
+  r1 = 'Ok, sounds good.';
+  r2 = 'No go.';
+  var var01 = (city == 'NYC') ? r1 : r2;
+  return var01;
+}
+
+switchOnCharmFromTip(tip) {
+  switch (expression) {
+    case expression:
+      
+      break;
+    case expression:
+      
+      break;
+      
+    default:
+      
+  }
+}
